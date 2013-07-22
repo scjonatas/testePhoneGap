e471caa0534c94e18f6face30aae6e3ffa4e6f20
scjonatas <scjonatas@gmail.com>
2013-07-22 17:23:59 -0300

    Fazendo outro hello world.

--- js/jqm-config.js
+++ js/jqm-config.js
@@ -1,20 +1,3 @@
 $(document).bind("mobileinit", function() {
 	$.mobile.defaultPageTransition = "slidefade";
-}); 
-
-function login() {
-    showWebPage('http://localhost/openid/index.php');
-    // $.jsonp({
-        // url: 'http://localhost/openid/index.php',
-        // callbackParameter: 'callback',
-        // success: function(data, status) {
-            // //$('#your-tweets').append('<li>The feed loads fine');
-            // $.each(data, function(i,item){
-                // $('#error').append('Msg = ' + item.msg);
-            // });
-        // },
-        // error: function(){
-            // $('#error').append('There was an error loading the feed');
-        // }
-    // });
-}
+});
