bc028cbfc807d2c8eb1a895d0ec24528ca82d551
scjonatas <scjonatas@gmail.com>
2013-07-23 17:30:38 -0300

    .

--- js/page2.js
+++ js/page2.js
@@ -1,3 +1,3 @@
-$(document).delegate("#page2", "pagechange", function() {
+$(document).delegate("#page2", "pageshow", function() {
 	alert('A page with an id of "page2" was just created by jQuery Mobile!');
 });
